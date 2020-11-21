bfc1a48d70a132365b78d52d93b045c508823a70
Etienne Batise <etienne.batise@gmail.com>
2020-11-21 22:17:38 +0100

    wip cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,9 @@
-use chrono::prelude::*;
-use num_rational::Rational32;
 use std::collections::VecDeque;
 use std::rc::{Rc, Weak};
 
-type Value = i32;
-type Weight = i32;
-// type UpperBound = Rational32;
-type UpperBound = i32;
+type Value = u32;
+type Weight = u32;
+type UpperBound = u32;
 
 struct Item {
     index: usize,
@@ -14,7 +11,7 @@ struct Item {
     weight: Weight,
 }
 
-fn compute_upper_bound<T>(items: &[(T, Value, Weight)], slack: Weight, value: Value) -> UpperBound {
+fn bound<T>(items: &[(T, Value, Weight)], slack: Weight, value: Value) -> UpperBound {
     if slack == 0 || items.len() == 0 {
         return value;
     }
@@ -35,23 +32,22 @@ fn compute_upper_bound<T>(items: &[(T, Value, Weight)], slack: Weight, value: Va
 #[derive(Debug, Clone)]
 struct Node {
     items: Vec<Weak<usize>>,
-    value: Value,
+    profit: Value,
     slack: Weight,
     level: usize,
 }
 
 fn crawl(items: &Vec<(Rc<usize>, Value, Weight)>, slack: Weight) -> (Vec<usize>, Value, Weight) {
-    // println!("{:?}", items);
     let mut queue: VecDeque<Node> = VecDeque::new();
     let init = Node {
         items: vec![],
-        value: 0,
+        profit: 0,
         slack: slack,
         level: 0,
     };
     let mut best_node = Node {
         items: vec![],
-        value: 0,
+        profit: 0,
         slack: slack,
         level: 0,
     };
@@ -60,10 +56,9 @@ fn crawl(items: &Vec<(Rc<usize>, Value, Weight)>, slack: Weight) -> (Vec<usize>,
         match queue.pop_front() {
             None => break,
             Some(n) => {
-                if n.value > best_node.value {
-                    // println!("Best node : {:?} {}", n.level, n.value);
+                if n.profit > best_node.profit {
                     best_node.items = n.items.to_vec();
-                    best_node.value = n.value;
+                    best_node.profit = n.profit;
                     best_node.slack = n.slack;
                     best_node.level = n.level;
                 }
@@ -71,12 +66,12 @@ fn crawl(items: &Vec<(Rc<usize>, Value, Weight)>, slack: Weight) -> (Vec<usize>,
                     continue;
                 }
                 let tail = &items[n.level..];
-                let local_upper_bound = compute_upper_bound(tail, n.slack, n.value);
-                if (local_upper_bound) > best_node.value {
+                let upper_bound = bound(tail, n.slack, n.profit);
+                if (upper_bound) > best_node.profit {
                     let new_node = Node {
                         items: n.items.to_vec(),
                         slack: n.slack,
-                        value: n.value,
+                        profit: n.profit,
                         level: n.level + 1,
                     };
                     queue.push_front(new_node);
@@ -88,7 +83,7 @@ fn crawl(items: &Vec<(Rc<usize>, Value, Weight)>, slack: Weight) -> (Vec<usize>,
                         let new_node = Node {
                             items: new_node_items.to_vec(),
                             slack: n.slack - w,
-                            value: n.value + v,
+                            profit: n.profit + v,
                             level: n.level + 1,
                         };
                         queue.push_front(new_node);
@@ -104,7 +99,7 @@ fn crawl(items: &Vec<(Rc<usize>, Value, Weight)>, slack: Weight) -> (Vec<usize>,
         resi.push(*x);
     }
 
-    (resi, best_node.value, best_node.slack)
+    (resi, best_node.profit, best_node.slack)
 }
 
 pub fn solve(items: &Vec<(Value, Weight)>, target: Weight) -> (Value, Weight, Vec<bool>) {
@@ -116,7 +111,6 @@ pub fn solve(items: &Vec<(Value, Weight)>, target: Weight) -> (Value, Weight, Ve
     sorted_items.sort_by(
         |(_, l_v, _, l_price_per_weight), (_, r_v, _, r_price_per_weight)| {
             (*r_price_per_weight, *r_v).cmp(&(*l_price_per_weight, *l_v))
-            // (*l_price_per_weight, *l_v).cmp(&(*r_price_per_weight, *r_v))
         },
     );
     let sorted_items = sorted_items
@@ -159,17 +153,17 @@ mod tests {
         let (v, w, res) = solve(&items, target);
         assert_eq!(v, 200);
         assert_eq!(w, 200);
-        // assert_eq!(
-        //     res,
-        //     vec![false, true, false, true, false, false, false, false, true, false]
-        // );
+        assert_eq!(
+            res,
+            vec![false, true, false, true, false, false, false, false, true, false]
+        );
     }
 
     #[test]
     fn test_crawl_with_empty_items() {
         let items = vec![];
-        let (_, v, _) = crawl(&items, 0);
-        // assert_eq!(best_node.items, vec![]);
+        let (i, v, _) = crawl(&items, 0);
+        assert_eq!(i, vec![]);
         assert_eq!(v, 0);
     }
 
@@ -177,8 +171,8 @@ mod tests {
     fn test_crawl_with_too_heavy_items() {
         let items = vec![(Rc::new(1), 1, 2), (Rc::new(2), 1, 2), (Rc::new(3), 1, 2)];
         let slack = 1;
-        let (_, v, _) = crawl(&items, slack);
-        // assert_eq!(best_node.items, vec![]);
+        let (i, v, _) = crawl(&items, slack);
+        assert_eq!(i, vec![]);
         assert_eq!(v, 0);
     }
 
@@ -187,8 +181,8 @@ mod tests {
     fn test_crawl_with_worthless_items() {
         let items = vec![(Rc::new(1), 0, 2), (Rc::new(2), 0, 2), (Rc::new(3), 0, 2)];
         let slack = 10;
-        let (_, v, _) = crawl(&items, slack);
-        // assert_eq!(best_node.items, vec![]);
+        let (i, v, _) = crawl(&items, slack);
+        assert_eq!(i, vec![]);
         assert_eq!(v, 0);
     }
 
@@ -198,22 +192,22 @@ mod tests {
         // weight is irrelevant for this check
         let items = vec![(Rc::new(1), 5, 0), (Rc::new(2), 6, 0)];
         let slack = 1;
-        let (_, v, _) = crawl(&items, slack);
+        let (i, v, _) = crawl(&items, slack);
         // We expect the best_node hasn't changed
-        // assert_eq!(best_node.items, vec![1, 2]);
+        assert_eq!(i, vec![1, 2]);
         assert_eq!(v, 11);
     }
 
     #[test]
     fn test_compute_upper_bound_when_slack_is_0() {
-        let res = compute_upper_bound::<usize>(&vec![], 0, 0);
-        assert_eq!(res, Rational32::from(0));
+        let res = bound::<usize>(&vec![], 0, 0);
+        assert_eq!(res, 0);
     }
 
     #[test]
     fn test_compute_upper_bound_when_vec_is_empty() {
-        let res = compute_upper_bound::<usize>(&vec![], 1, 1);
-        assert_eq!(res, Rational32::from(1));
+        let res = bound::<usize>(&vec![], 1, 1);
+        assert_eq!(res, 1);
     }
 
     #[test]
@@ -222,8 +216,8 @@ mod tests {
         // Here usizes are not important
         let items = vec![(1, 1, 1), (2, 1, 1), (3, 1, 1)];
         // Only 1 weight unit remaining, value is 1
-        let res = compute_upper_bound(&items, 1, 1);
+        let res = bound(&items, 1, 1);
         // expected upper bound is 2
-        assert_eq!(res, Rational32::from(2));
+        assert_eq!(res, 2);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,20 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
-fn parse_file(path: String) -> std::io::Result<(i32, usize, Vec<i32>)> {
+fn parse_file(path: String) -> std::io::Result<(u32, usize, Vec<u32>)> {
     let mut content = String::new();
     File::open(path)?.read_to_string(&mut content)?;
     let lines: Vec<_> = content.lines().collect();
 
     let line1 = lines[0];
     let line1_split: Vec<_> = line1.split_ascii_whitespace().collect();
-    let max = line1_split[0].parse::<i32>().unwrap();
+    let max = line1_split[0].parse::<u32>().unwrap();
     let n = line1_split[1].parse::<usize>().unwrap();
 
     let line2 = lines[1];
-    let mut items: Vec<i32> = vec![];
+    let mut items: Vec<u32> = vec![];
     for i in line2.split_ascii_whitespace().collect::<Vec<&str>>() {
-        items.push(i.parse::<i32>().unwrap());
+        items.push(i.parse::<u32>().unwrap());
     }
     return Ok((max, n, items));
 }
