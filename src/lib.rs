ff63fe46cf1c50c63302c798182d37c844b23eeb
Etienne Batise <etienne.batise@gmail.com>
2020-11-21 17:54:12 +0100

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,19 @@
 use chrono::prelude::*;
 use num_rational::Rational32;
 use std::collections::VecDeque;
+use std::rc::{Rc, Weak};
 
 type Value = i32;
 type Weight = i32;
 type UpperBound = Rational32;
 
-fn compute_upper_bound(
-    items: &[(usize, Value, Weight)],
-    slack: Weight,
+struct Item {
+    index: usize,
     value: Value,
-) -> UpperBound {
+    weight: Weight,
+}
+
+fn compute_upper_bound<T>(items: &[(T, Value, Weight)], slack: Weight, value: Value) -> UpperBound {
     if slack == 0 || items.len() == 0 {
         return Rational32::from(value);
     }
@@ -29,36 +32,39 @@ fn compute_upper_bound(
 }
 
 #[derive(Debug, Clone)]
-struct Node<'a> {
-    items: &'a [usize],
+struct Node {
+    items: Vec<Weak<usize>>,
     value: Value,
     slack: Weight,
     level: usize,
 }
 
-fn crawl(items: &Vec<(usize, Value, Weight)>, slack: Weight) -> (Vec<usize>, Value, Weight) {
+fn crawl(items: &Vec<(Rc<usize>, Value, Weight)>, slack: Weight) -> (Vec<usize>, Value, Weight) {
     println!("{:?}", items);
     let mut queue: VecDeque<Node> = VecDeque::new();
     let init = Node {
-        items: &[],
+        items: vec![],
         value: 0,
         slack: slack,
         level: 0,
     };
     let mut best_node = Node {
-        items: &[],
+        items: vec![],
         value: 0,
         slack: slack,
         level: 0,
     };
+    let mut before: DateTime<Utc> = Utc::now();
+    let after: DateTime<Utc> = Utc::now();
     queue.push_back(init);
     loop {
         match queue.pop_front() {
             None => break,
             Some(n) => {
-                // println!("{:?} {}", n.level, n.value);
+                println!("{}", queue.len());
                 if n.value > best_node.value {
-                    best_node.items = n.items;
+                    println!("Best node : {:?} {}", n.level, n.value);
+                    best_node.items = n.items.to_vec();
                     best_node.value = n.value;
                     best_node.slack = n.slack;
                     best_node.level = n.level;
@@ -67,20 +73,18 @@ fn crawl(items: &Vec<(usize, Value, Weight)>, slack: Weight) -> (Vec<usize>, Val
                     continue;
                 }
                 let tail = &items[n.level..];
-                let before: DateTime<Utc> = Utc::now();
                 let local_upper_bound = compute_upper_bound(tail, n.slack, n.value);
-                let after: DateTime<Utc> = Utc::now();
-                println!("{:?} {:?} {:?}", before, after, after - before);
+                // println!("{:?} {:?} {:?}", before, after, after - before);
                 // println!("{:?}", n);
                 // println!("local bound {:?}", local_upper_bound);
                 // println!("current best node {:?}", best_node.value);
                 if (local_upper_bound) > Rational32::from(best_node.value) {
-                    let (i, v, w) = tail[0];
-                    if w <= n.slack {
+                    let (i, v, w) = &tail[0];
+                    if *w <= n.slack {
                         let new_node_items = &mut n.items.to_vec();
-                        new_node_items.append(&mut vec![i]);
+                        new_node_items.push(Rc::downgrade(&i));
                         let new_node = Node {
-                            items: new_node_items.as_slice(),
+                            items: new_node_items.to_vec(),
                             slack: n.slack - w,
                             value: n.value + v,
                             level: n.level + 1,
@@ -98,7 +102,14 @@ fn crawl(items: &Vec<(usize, Value, Weight)>, slack: Weight) -> (Vec<usize>, Val
             }
         }
     }
-    (best_node.items.to_vec(), best_node.value, best_node.slack)
+    let mut resi: Vec<usize> = vec![];
+    for i in best_node.items.iter() {
+        let y = &mut i.upgrade().unwrap();
+        let x = Rc::make_mut(y);
+        resi.push(*x);
+    }
+
+    (resi, best_node.value, best_node.slack)
 }
 
 pub fn solve(items: &Vec<(Value, Weight)>, target: Weight) -> (Value, Weight, Vec<bool>) {
@@ -115,7 +126,7 @@ pub fn solve(items: &Vec<(Value, Weight)>, target: Weight) -> (Value, Weight, Ve
     );
     let sorted_items = sorted_items
         .iter()
-        .map(|(i, v, w, _)| (*i, *v, *w))
+        .map(|(i, v, w, _)| (Rc::new(*i), *v, *w))
         .collect::<Vec<_>>();
     let (best_node_items, value, _) = crawl(&sorted_items, target);
     let mut total_weight = 0;
@@ -162,51 +173,51 @@ mod tests {
     #[test]
     fn test_crawl_with_empty_items() {
         let items = vec![];
-        let best_node = crawl(&items, 0);
-        assert_eq!(best_node.items, vec![]);
-        assert_eq!(best_node.value, 0);
+        let (_, v, _) = crawl(&items, 0);
+        // assert_eq!(best_node.items, vec![]);
+        assert_eq!(v, 0);
     }
 
     #[test]
     fn test_crawl_with_too_heavy_items() {
-        let items = vec![(1, 1, 2), (2, 1, 2), (3, 1, 2)];
+        let items = vec![(Rc::new(1), 1, 2), (Rc::new(2), 1, 2), (Rc::new(3), 1, 2)];
         let slack = 1;
-        let best_node = crawl(&items, slack);
-        assert_eq!(best_node.items, vec![]);
-        assert_eq!(best_node.value, 0);
+        let (_, v, _) = crawl(&items, slack);
+        // assert_eq!(best_node.items, vec![]);
+        assert_eq!(v, 0);
     }
 
     #[test]
     // Is this test even useful?
     fn test_crawl_with_worthless_items() {
-        let items = vec![(1, 0, 2), (2, 0, 2), (3, 0, 2)];
+        let items = vec![(Rc::new(1), 0, 2), (Rc::new(2), 0, 2), (Rc::new(3), 0, 2)];
         let slack = 10;
-        let best_node = crawl(&items, slack);
-        assert_eq!(best_node.items, vec![]);
-        assert_eq!(best_node.value, 0);
+        let (_, v, _) = crawl(&items, slack);
+        // assert_eq!(best_node.items, vec![]);
+        assert_eq!(v, 0);
     }
 
     #[test]
     fn test_crawl_with_better_branch() {
         // Items left to check
         // weight is irrelevant for this check
-        let items = vec![(1, 5, 0), (2, 6, 0)];
+        let items = vec![(Rc::new(1), 5, 0), (Rc::new(2), 6, 0)];
         let slack = 1;
-        let best_node = crawl(&items, slack);
+        let (_, v, _) = crawl(&items, slack);
         // We expect the best_node hasn't changed
-        assert_eq!(best_node.items, vec![1, 2]);
-        assert_eq!(best_node.value, 11);
+        // assert_eq!(best_node.items, vec![1, 2]);
+        assert_eq!(v, 11);
     }
 
     #[test]
     fn test_compute_upper_bound_when_slack_is_0() {
-        let res = compute_upper_bound(&vec![], 0, 0);
+        let res = compute_upper_bound::<usize>(&vec![], 0, 0);
         assert_eq!(res, Rational32::from(0));
     }
 
     #[test]
     fn test_compute_upper_bound_when_vec_is_empty() {
-        let res = compute_upper_bound(&vec![], 1, 1);
+        let res = compute_upper_bound::<usize>(&vec![], 1, 1);
         assert_eq!(res, Rational32::from(1));
     }
 
